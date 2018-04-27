71f6c6a4fbaca7f18b1700a2d3546d9b5afb5cfd
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-04-27 19:02:39 +0200

    RemoveNodeListener.kt RemoveNodeListenerTest.kt

---
+++ src/main/kotlin/pl/grzeslowski/colibra/graph/listeners/RemoveNodeListener.kt
@@ -0,0 +1,31 @@
+package pl.grzeslowski.colibra.graph.listeners
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import pl.grzeslowski.colibra.graph.GraphRepository
+import pl.grzeslowski.colibra.graph.Node
+import pl.grzeslowski.colibra.graph.NodeNotFound
+import pl.grzeslowski.colibra.server.ClientMessage
+import pl.grzeslowski.colibra.server.ColibraChannel
+import pl.grzeslowski.colibra.server.NewMessageListener
+import pl.grzeslowski.colibra.server.ServerMessage
+
+@Component
+class RemoveNodeListener(private val graphRepository: GraphRepository) : NewMessageListener {
+    private val logger = LoggerFactory.getLogger(RemoveNodeListener::class.java)
+    private val prefix = "REMOVE NODE "
+    override fun onNewMessage(clientMessage: ClientMessage, channel: ColibraChannel): Boolean =
+            if (clientMessage.message.startsWith(prefix)) {
+                val nodeName = clientMessage.message.substring(prefix.length)
+                try {
+                    graphRepository.addNode(Node(nodeName))
+                    channel.write(ServerMessage("NODE REMOVED"))
+                } catch (ex: NodeNotFound) {
+                    logger.error("Cannot remove  node", ex)
+                    channel.write(ServerMessage("ERROR: NODE NOT FOUND"))
+                }
+                true
+            } else {
+                false
+            }
+}
---
+++ src/test/kotlin/pl/grzeslowski/colibra/graph/listeners/RemoveNodeListenerTest.kt
@@ -0,0 +1,93 @@
+package pl.grzeslowski.colibra.graph.listeners
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import pl.grzeslowski.colibra.graph.GraphRepository
+import pl.grzeslowski.colibra.graph.Node
+import pl.grzeslowski.colibra.graph.NodeNotFound
+import pl.grzeslowski.colibra.server.ClientMessage
+import pl.grzeslowski.colibra.server.ColibraChannel
+import pl.grzeslowski.colibra.server.ServerMessage
+import pl.grzeslowski.colibra.spring.testProfileName
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@ActiveProfiles(testProfileName)
+internal class RemoveNodeListenerTest {
+    @Autowired
+    lateinit var listener: RemoveNodeListener
+    @MockBean
+    lateinit var graphRepository: GraphRepository
+
+    lateinit var channel: ColibraChannel
+
+    @BeforeEach
+    internal fun setUp() {
+        channel = Mockito.mock(ColibraChannel::class.java)
+    }
+
+    @Test
+    fun `should return false if message is not in proper format`() {
+
+        // given
+        val message = ClientMessage("REMOVE SMTH")
+
+        // when
+        val onNewMessage = listener.onNewMessage(message, channel)
+
+        // then
+        Assertions.assertThat(onNewMessage).isFalse()
+    }
+
+    @Test
+    fun `should return true if message is in proper format`() {
+
+        // given
+        val message = ClientMessage("REMOVE NODE TEST-NODE")
+
+        // when
+        val onNewMessage = listener.onNewMessage(message, channel)
+
+        // then
+        Assertions.assertThat(onNewMessage).isTrue()
+    }
+
+    @Test
+    fun `should return add node to repository`() {
+
+        // given
+        val nodeName = "TEST-NODE"
+        val message = ClientMessage("REMOVE NODE $nodeName")
+
+        // when
+        listener.onNewMessage(message, channel)
+
+        // then
+        Mockito.verify(graphRepository).addNode(Node(nodeName))
+    }
+
+    @Test
+    fun `should write message if node is duplicate`() {
+
+        // given
+        val nodeName = "TEST-NODE"
+        val message = ClientMessage("REMOVE NODE $nodeName")
+        val node = Node(nodeName)
+        BDDMockito.given(graphRepository.addNode(node)).willThrow(NodeNotFound(node))
+
+        // when
+        listener.onNewMessage(message, channel)
+
+        // then
+        Mockito.verify(channel).write(ServerMessage("ERROR: NODE NOT FOUND"))
+    }
+}
