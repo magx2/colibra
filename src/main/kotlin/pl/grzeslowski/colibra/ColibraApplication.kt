596da7383497cac3c39001140ede3c939acb21c0
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-04-27 20:38:00 +0200

    Removing not needed class

--- src/main/kotlin/pl/grzeslowski/colibra/ColibraApplication.kt
+++ src/main/kotlin/pl/grzeslowski/colibra/ColibraApplication.kt
@@ -1,27 +1,11 @@
 package pl.grzeslowski.colibra
 
-import org.slf4j.LoggerFactory
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Component
-import pl.grzeslowski.colibra.spring.NotTestProfile
-import java.util.concurrent.TimeUnit
 
 @SpringBootApplication
 class ColibraApplication
 
 fun main(args: Array<String>) {
     runApplication<ColibraApplication>(*args)
-}
-
-@NotTestProfile
-@Component
-class RuIt : CommandLineRunner {
-    private val log = LoggerFactory.getLogger(RuIt::class.java)
-    override fun run(vararg args: String?) {
-        TimeUnit.MINUTES.sleep(10)
-        log.info("Closing...")
-    }
-
 }
