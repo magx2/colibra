2366c669818fbdd38ad1c0f71369651ac6638c1b
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-04-28 23:19:22 +0200

    Moving from naive queue to linked list priority queue

--- src/main/kotlin/pl/grzeslowski/colibra/graph/RouteFinder.kt
+++ src/main/kotlin/pl/grzeslowski/colibra/graph/RouteFinder.kt
@@ -1,6 +1,7 @@
 package pl.grzeslowski.colibra.graph
 
 import org.springframework.stereotype.Service
+import java.util.*
 
 interface RouteFinder {
     fun shortestPath(graph: Graph, from: Node, to: Node): Int
@@ -20,26 +21,13 @@ class RouteFinderImpl : RouteFinder {
         graph.nodes().forEach { node -> distances[node] = undefined }
         distances[from] = 0
 
-        var current = from
-        while (unvisitedSet.isNotEmpty()) {
-            val currentDistance = distances[current]!!
-            unvisitedNeighbors(current, unvisitedSet, graph)
-                    .forEach { unvisitedNeighbor ->
-                        val unvisitedNeighborDistance = distances[unvisitedNeighbor.node]!!
-                        val newDistance = currentDistance + unvisitedNeighbor.weight
-                        if (newDistance < unvisitedNeighborDistance) {
-                            distances[unvisitedNeighbor.node] = newDistance
-                        }
-                    }
-            unvisitedSet.remove(current)
-            val newCurrent = distances.entries
-                    .stream()
-                    .filter { entry -> unvisitedSet.contains(entry.key) }
-                    .sorted { o1, o2 -> o1.value.compareTo(o2.value) }
-                    .map { it.key }
-                    .findFirst()
-            if (newCurrent.isPresent) {
-                current = newCurrent.get()
+        var queue: Element? = initQueue(Element(from, null, null), graph)
+
+        while (unvisitedSet.isNotEmpty() && queue != null) {
+            val current = queue.node
+            if (queue.next != null) {
+                queue = queue.next!!
+                queue.prev = null
             } else {
                 break
             }
@@ -51,13 +39,115 @@ class RouteFinderImpl : RouteFinder {
             if (current == to) {
                 break
             }
+
+            val currentDistance = distances[current]!!
+            unvisitedNeighbors(current, unvisitedSet, graph)
+                    .forEach { unvisitedNeighbor ->
+                        val unvisitedNeighborDistance = distances[unvisitedNeighbor.node]!!
+                        val newDistance = currentDistance + unvisitedNeighbor.weight
+                        if (newDistance < unvisitedNeighborDistance) {
+                            queue = setNewDistance(distances, unvisitedNeighbor.node, newDistance, queue!!)
+                        }
+                    }
+            unvisitedSet.remove(current)
         }
 
         return distances[to]!!
+    }
+
+    private fun initQueue(queue: Element, graph: Graph): Element {
+        var last = queue
+        graph.nodes().forEach { node ->
+            if (node != queue.node) {
+                val new = Element(node, last, null)
+                last.next = new
+                last = new
+            }
+        }
+        return queue
+    }
+
+    private fun remove(queue: Element, node: Node): Element? {
+        var element: Element? = queue
+        while (true) {
+            when {
+                element == null -> return null
+                element.node == node -> return when {
+                    element.prev == null -> queue.next
+                    element.next == null -> {
+                        element.prev = null
+                        queue
+                    }
+                    else -> {
+                        val prev = element.prev!!
+                        val next = element.next!!
+                        prev.next = next
+                        next.prev = prev
+                        queue
+                    }
+                }
+                else -> element = element.next
+            }
+        }
+    }
+
+    private fun setNewDistance(distances: HashMap<Node, Int>, unvisitedNeighbor: Node, newDistance: Int, queue: Element): Element {
+        distances[unvisitedNeighbor] = newDistance
+
+        val nodeInQueue = findNodeInQueue(queue, unvisitedNeighbor)
+
+
+        while (true) {
+            if (nodeInQueue.prev == null) {
+                return nodeInQueue
+            }
+            val prev = nodeInQueue.prev!!
+            val prevDistance = distances[prev.node]!!
+            if (prevDistance > newDistance) {
+                val oldNext = nodeInQueue.next
+                val oldPrevPrev = prev.prev
+
+                nodeInQueue.prev = oldPrevPrev
+                nodeInQueue.next = prev
+                if (oldPrevPrev != null) {
+                    oldPrevPrev.next = nodeInQueue
+                }
+
+                prev.prev = nodeInQueue
+                prev.next = oldNext
+
+                if (oldNext != null) {
+                    oldNext.prev = prev
+                }
+                if (oldPrevPrev != null) {
+                    oldPrevPrev.next = nodeInQueue
+                }
+            } else {
+                var returnQueue = nodeInQueue
+                while (returnQueue.prev != null) {
+                    returnQueue = returnQueue.prev!!
+                }
+                return returnQueue
+            }
+        }
+    }
+
+    private fun findNodeInQueue(queue: Element, unvisitedNeighbor: Node): Element {
+        var nodeInQueue: Element = queue
+        while (nodeInQueue.node != unvisitedNeighbor) {
+            nodeInQueue = nodeInQueue.next!!
+        }
+        return nodeInQueue
     }
 
     private fun unvisitedNeighbors(node: Node, unvisitedSet: HashSet<Node>, graph: Graph) =
             graph.adjacencyList[node]!!
                     .stream()
                     .filter { neighbour -> unvisitedSet.contains(neighbour.node) }
+}
+
+private data class Element(val node: Node, var prev: Element?, var next: Element?) {
+    override fun toString(): String {
+        return "Element($node, prev = ${prev != null}, next = ${next != null})"
+    }
 }
