6d98d931763d044ea4b6e79154d1357a95d1fea3
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-04-29 11:09:09 +0200

    Removing unused imports vol. 2

--- src/main/kotlin/pl/grzeslowski/colibra/chat/HelloListener.kt
+++ src/main/kotlin/pl/grzeslowski/colibra/chat/HelloListener.kt
@@ -1,12 +1,8 @@
 package pl.grzeslowski.colibra.chat
 
-import io.netty.channel.Channel
-import jdk.nashorn.internal.runtime.regexp.joni.Config.log
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
 import pl.grzeslowski.colibra.server.ColibraChannel
 import pl.grzeslowski.colibra.server.NewChannelListener
-import pl.grzeslowski.colibra.server.NewMessageListener
 import pl.grzeslowski.colibra.server.ServerMessage
 
 @Component
